53b22148b1acbb32e64ae9d3c3a36f58e9451839
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-06-23 16:52:58 +0100

    Update mass_spectrometry.py

--- sequal/mass_spectrometry.py
+++ sequal/mass_spectrometry.py
@@ -4,13 +4,15 @@ ax = "ax"
 by = "by"
 cz = "cz"
 
+
 # calculate non-labile modifications and yield associated transition
 # For example "by" would yield a tuple of "b" and "y" transitions.
 def fragment_non_labile(sequence, fragment_type):
     for i in range(1, sequence.seq_length, 1):
         left = Ion(sequence[:i], fragment_number=i, ion_type=fragment_type[0])
-        right = Ion(sequence[i:], fragment_number=sequence.seq_length-i, ion_type=fragment_type[1])
+        right = Ion(sequence[i:], fragment_number=sequence.seq_length - i, ion_type=fragment_type[1])
         yield left, right
+
 
 # calculate all labile modification variants for the sequence and its associated labile modifications
 def fragment_labile(sequence):
@@ -32,6 +34,3 @@ class FragmentFactory:
 
     def set_ignore(self, ignore):
         self.ignore = ignore
-
-
-
