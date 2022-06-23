489a31574b1bb8025a486fd07e45ccff5166bf38
Toan Phung <toan.phungkhoiquoctoan@gmail.com>
2022-06-23 05:59:56 +0100

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup
 
-setup(
-    name='sequal',
-    version='0.0.1',
-    packages=['sequal'],
-    url='https://github.com/noatgnu/sequal',
-    license='MIT',
-    author='Toan K. Phung',
-    author_email='toan.phung@acryno.me',
-    description='A python package for formating and parsing biological sequence with modifications'
-)
+
+if __name__ == '__main__':
+    setup()
